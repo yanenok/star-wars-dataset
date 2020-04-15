79ecd60a5f88610341eba2d72b2cb517f7eba9dd
Yana <yanenok@gmail.com>
2020-04-15 16:52:39 +0300

    Added DDL

---
+++ ddl/people_vehicles.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "people_vehicles" (
+  "people_id" INTEGER,
+  "vehicle_id" INTEGER
+);
+
+INSERT INTO people_vehicles (people_id, vehicle_id) VALUES
+(1, 14),
+(1, 30),
+(5, 30),
+(10, 38),
+(11, 44),
+(11, 46),
+(13, 19),
+(18, 14),
+(32, 38),
+(44, 42),
+(67, 55),
+(70, 45),
+(79, 60);
